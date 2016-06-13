2357b3a278aaeac5a5553bc523bda3886d37f264
Daniele Cebin <danielecebin@hotmail.com>
2016-06-13 00:22:54 -0300

    Semana 3: TDD - Refactor e organização em pacotes main/test

---
+++ Proj2-QualPrecoDaPizza/src/main/Principal.java
@@ -0,0 +1,53 @@
+package main;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+	Pizza nutellaComBanana = new Pizza();
+	nutellaComBanana.adicionaIngrediente("nutella");
+	nutellaComBanana.adicionaIngrediente("banana");
+	nutellaComBanana.setApelido("Nutella com banana"); 
+
+	Pizza marguerita = new Pizza();
+	marguerita.adicionaIngrediente("molho");
+	marguerita.adicionaIngrediente("mozarela");
+	marguerita.adicionaIngrediente("manjericao");
+
+	Pizza caipira = new Pizza();
+	caipira.adicionaIngrediente("molho");
+	caipira.adicionaIngrediente("frango");
+	caipira.adicionaIngrediente("catupiry");
+	caipira.adicionaIngrediente("milho");
+	caipira.adicionaIngrediente("mozarela");
+
+	Pizza frangoRequeijao = new Pizza();
+	frangoRequeijao.adicionaIngrediente("molho");
+	frangoRequeijao.adicionaIngrediente("frango");
+	frangoRequeijao.adicionaIngrediente("catupiry");
+	frangoRequeijao.adicionaIngrediente("milho");
+	frangoRequeijao.adicionaIngrediente("mozarela");
+	frangoRequeijao.adicionaIngrediente("requeijao");
+
+	Pizza vazia = new Pizza();
+	vazia.setApelido("vazia");
+
+	CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+	carrinho.adicionaPizza(nutellaComBanana);
+	carrinho.adicionaPizza(marguerita);
+	carrinho.adicionaPizza(caipira);
+	carrinho.adicionaPizza(frangoRequeijao);
+	carrinho.adicionaPizza(vazia);
+
+	/* Relatorio */
+	carrinho.listaPizzas();
+	System.out.printf("\nValor da compra:\n\tR$ %.2f \n", (double) carrinho.valorTotal());
+
+	Pizza.imprimeIngredientesUtilizados();
+	
+	
+	
+
+    }
+
+}
